9b53b5383fd5b518972e3e2252dc99c2e5acdcec
nicksirias <nick.sirias@yale.edu>
2025-11-14 09:35:05 -0500

    Revert problematic commit

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("signin/", views.signin, name="signin"),
-    path("logout/", views.logout, name="logout"),
-    path("events/create/", views.create_event, name="create_event"),
-    path("events/<int:event_id>/", views.event_detail, name="event_detail"),
-    path("events/<int:event_id>/rsvp/", views.rsvp, name="rsvp"),
+    path("logout/", views.logout, name="logout"),  # new
 ]
