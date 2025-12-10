96334ae4bbda0dfb86dae2729248d8e2a522d522
itay-rubin <itayrubin15@gmail.com>
2025-12-10 17:28:41 -0500

    added hashing to core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+import hashlib
+
+TEAM_NICKNAME = "icy-jungle"
+ABTEST_SLUG = hashlib.sha1(TEAM_NICKNAME.encode()).hexdigest()[:7]
+
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -7,4 +12,5 @@ urlpatterns = [
     path("logout/", views.logout, name="logout"),
     path("host-event/", views.host_event, name="host_event"),
     path("rsvp-toggle/<str:event_id>/", views.rsvp_toggle, name="rsvp_toggle"),
+    path(f"{ABTEST_SLUG}/", views.abtest, name="abtest"),  # <- public A/B endpoint
 ]
